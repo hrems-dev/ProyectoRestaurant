265f2f8dfb74f7318aaf388eb5d5883fb0c00cbf
hrems-dev <hrems077@gmail.com>
2025-06-03 10:32:37 -0500

    estrucuture de proyecto

---
+++ ProyectoRestaurant/src/main/java/pe/edu/upeu/proyecto_restaurant/ProyectoRestaurantApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.proyecto_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRestaurantApplication.class, args);
+	}
+
+}
---
+++ ProyectoRestaurant/src/test/java/pe/edu/upeu/proyecto_restaurant/ProyectoRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.proyecto_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
