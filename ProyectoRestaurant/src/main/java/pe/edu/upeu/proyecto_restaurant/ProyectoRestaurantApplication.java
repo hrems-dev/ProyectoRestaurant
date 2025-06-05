f29ca42bee1ee454383c5ebb7843c2d9ba7abf1e
hrems-dev <hrems077@gmail.com>
2025-06-05 18:32:09 -0500

    interfas de menu

--- ProyectoRestaurant/src/main/java/pe/edu/upeu/proyecto_restaurant/ProyectoRestaurantApplication.java
+++ ProyectoRestaurant/src/main/java/pe/edu/upeu/proyecto_restaurant/ProyectoRestaurantApplication.java
@@ -1,16 +1,11 @@
 package pe.edu.upeu.proyecto_restaurant;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.kordamp.bootstrapfx.BootstrapFX;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -22,35 +17,45 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.util.List;
 
 @SpringBootApplication
-public class ProyectoRestaurantApplication extends Application{
+public class ProyectoRestaurantApplication extends Application {
 	private static ConfigurableApplicationContext configurableApplicationContext;
 	private Parent parent;
+
 	public static void main(String[] args) {
 		//SpringApplication.run(ProyectoRestaurantApplication.class, args);
 		launch(args);
 	}
+
 	@Override
 	public void init() throws Exception {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(ProyectoRestaurantApplication.class);
 		builder.application().setWebApplicationType(WebApplicationType.NONE);
-		configurableApplicationContext = builder.run(getParameters().getRaw().toArray(new String[0])); // para
-		//FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/main_producto.fxml"));
+		configurableApplicationContext = builder.run(getParameters().getRaw().toArray(new String[0]));
+		
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/admMenu.fxml"));
 		fxmlLoader.setControllerFactory(configurableApplicationContext::getBean);
-		parent= fxmlLoader.load();
+		parent = fxmlLoader.load();
 	}
-	 @Override
+
+	@Override
 	public void start(Stage stage) throws Exception {
 		Scene scene = new Scene(parent);
-		scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-		scene.getStylesheets().add(getClass().getResource("/views/css/styles.css").toExternalForm());
+		
+		// Add custom CSS if exists
+		var cssResource = getClass().getResource("/css/styles.css");
+		if (cssResource != null) {
+			scene.getStylesheets().add(cssResource.toExternalForm());
+		}
+		
 		stage.setScene(scene);
-		stage.setTitle("SysAlmacen Spring Java-FX");
+		stage.setTitle("Sistema Restaurante");
 		stage.setResizable(false);
 		stage.show();
-	} 
+	}
 
-
-
-
+	@Override
+	public void stop() throws Exception {
+		configurableApplicationContext.close();
+		Platform.exit();
+	}
 }
